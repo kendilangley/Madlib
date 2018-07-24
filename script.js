ff8751f9a3607bbe2460d897897a1030f811cc04
DePaul <CONF0581@depaul.edu>
2018-07-24 15:35:59 -0500

    <kendi >

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	
+	$("button").click(function() {
+		console.log("kendi");   
+
+		var Adjective = $("#Adjective1").val();
+		console.log(Adjective);
+		
+        var Adjective2 = $("#Adjective2").val();
+		console.log(Adjective2);
+		
+		var Job = $("#Job").val();
+		console.log(Job)
+		
+		var 2nd Name = $("#2nd Name").val();
+		console.log(2nd Name)
+		
+	});
+});
